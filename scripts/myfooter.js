4b36a7c41137c6680e815a4dc35e156079576272
yinyicao <yinyicao@users.noreply.github.com>
2020-10-10 09:56:40 +0000

    deploy: 91f4241f46f15fc8cd60b904a74dd5921ab1ab02

---
+++ scripts/myfooter.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+
+    // ICP备案信息节点
+    var record = document.getElementsByClassName("footer-wrapper footer")[0].children[1]
+    // 创建一个span节点，并填充数据，用于显示公安备案信息
+    var cyberSecurityRecord = document.createElement('span')
+    cyberSecurityRecord.innerHTML=document.getElementsByClassName("footer-wrapper footer")[0].children[4].innerHTML
+    // 设置classname
+    cyberSecurityRecord.className = "cyber-security"
+    // 获取并加上自定义的属性名称
+    cyberSecurityRecord.setAttribute(document.getElementsByClassName("footer-wrapper footer")[0].firstElementChild.getAttributeNames()[0],"")
+    
+    // 清空document.getElementsByClassName("footer-wrapper footer")[0]节点，加入自定义的属性
+    document.getElementsByClassName("footer-wrapper footer")[0].innerHTML=""
+    document.getElementsByClassName("footer-wrapper footer")[0].appendChild(record)
+    document.getElementsByClassName("footer-wrapper footer")[0].appendChild(cyberSecurityRecord)
+    
+}
+
+
+
