9d105cdb199504644a771ce51c49ac70711cd9af
yinyicao <yinyicao@foxmail.com>
2022-05-26 14:01:07 +0800

    Revert "rollback proxy setting"
    This reverts commit c6e644f679c45c4bcb19af4ce354006275f79027.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -69,7 +69,8 @@ module.exports = {
             owner: 'yinyicao',
             repo: 'yinyicao.github.io',
             clientId: '8e13003e6b6d3db2dfe3',
-            clientSecret: '89926d894f5f5166a9b421f668234b8f81b42e0c'
+            clientSecret: '89926d894f5f5166a9b421f668234b8f81b42e0c',
+			proxy: url => `https://cors-anywhere.azm.workers.dev/${url}`
         },
         // 备案
         record: '渝ICP备19002727号-2',
