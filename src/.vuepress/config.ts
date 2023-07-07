9d41173bb7cd1b3ca8505e45d5ec8a1db43ea5bb
yinyicao <yinyicao@foxmail.com>
2023-07-07 15:46:59 +0800

    升级版本、增加重定向
    1. 升级框架版本
    2. 增加页面重定向，解决改版之前的页面访问不到的问题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { redirectPlugin } from "vuepress-plugin-redirect";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -22,6 +23,19 @@ export default defineUserConfig({
     searchProPlugin({
       // 索引全部内容
       indexContent: true,
+    }),
+    redirectPlugin({
+      // 重定向配置
+      config: (app) =>{
+        // 如果是/posts/开头的页面就设置一条规则[/a,/posts/a],表示访问/a时重定向到/posts/a
+        const redirects  = app.pages
+        .filter(({ path }) => path.startsWith("/posts/"))
+        .map(({ path }) => [path.replace(/^\/posts\//, "/"), path]);
+        // console.log(redirects)
+        return Object.fromEntries(
+          redirects
+        )
+      },
     }),
   ],
 
